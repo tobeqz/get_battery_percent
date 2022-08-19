9ca12effd0c5536b0bb670e475816f74abfb0187
Jorik <vanveenjorik@gmail.com>
2022-08-19 11:19:08 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+const BATTERY_PATH: &'static str = "/sys/class/power_supply/BAT0/";
+
+fn read_battery_file(file: &str, strip_last_char: bool) -> String {
+    use std::fs::File;
+    use std::io::prelude::*;
+    let mut full_path = String::from(BATTERY_PATH);
+    full_path.push_str(file);
+
+    let mut contents = String::new();
+    let mut file = File::open(full_path).expect("Unable to open battery file");
+    file.read_to_string(&mut contents).expect("Unable to read file");
+
+    if strip_last_char {
+        contents.pop();
+    }
+
+    contents
+}
+
+fn parse_int(string: String) -> i32 {
+    string.parse::<i32>().expect("Unable to parse int")
+}
+
+fn main() {
+    let energy_full = parse_int(read_battery_file("energy_full", true));
+    let energy_now = parse_int(read_battery_file("energy_now", true));
+    let power_now = parse_int(read_battery_file("power_now", true));
+
+    let percentage = ((energy_now as f32) / (energy_full as f32) * 100.0).round();
+
+    let status = read_battery_file("status", true);
+    let status_short: &str;
+
+    let time_remaining = match status.as_str() {
+        "Charging" => {
+            let remaining_energy = energy_full - energy_now;
+            status_short = "C";
+            (remaining_energy as f32) / (power_now as f32)
+        },
+        "Discharging" => {
+            let remaining_energy = energy_now;
+            status_short = "D";
+            (remaining_energy as f32) / (power_now as f32)
+        },
+        _ => panic!("Unknown battery status")
+    };
+
+    let time_remaining_split: Vec<String> = time_remaining.to_string().split(".").map(|a| String::from(a)).collect();
+    let hours_part = &time_remaining_split[0];
+    let frac_part = &time_remaining_split[1][0..1];
+    let time_remaining = format!("{}.{}", hours_part, frac_part);
+
+    println!("{}% {} {}h", percentage, status_short, time_remaining);
+}
